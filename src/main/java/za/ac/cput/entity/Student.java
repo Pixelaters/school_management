cfb3ad787b3831906d36707ae1945be75296f797
Raeece <raeecesamuels100@gmail.com>
2022-06-10 14:54:19 +0200

    Update Student.java

--- src/main/java/za/ac/cput/entity/Student.java
+++ src/main/java/za/ac/cput/entity/Student.java
@@ -1,4 +1,74 @@
 package za.ac.cput.entity;
 
+/* Name: Raeece Samuels(217283764)
+
+ */
+
 public class Student {
-}
+
+        private final String studentId, email;
+        private final Name name;
+
+        private Student(Builder b) {
+            this.studentId = b.studentId;
+            this.email = b.email;
+            this.name = b.name;
+        }
+
+        public String getStudentId() {
+            return studentId;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public Name getName() {
+            return name;
+        }
+
+        public static class Builder{
+            private String studentId, email;
+            private Name name;
+
+            public Builder setStudentId(String studentId) {
+                this.studentId = studentId;
+                return this;
+            }
+
+            public Builder setEmail(String email) {
+                this.email = email;
+                return this;
+            }
+
+
+
+            public Builder copy(Student s){
+                this.studentId = s.studentId;
+                this.email = s.email;
+                this.name = s.getName();
+                return this;
+
+            }
+
+            public Student build(){
+                return new Student(this);
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return super.equals(obj);
+            }
+
+            @Override
+            public String toString() {
+                return "Builder{" +
+                        "studentId='" + studentId + '\'' +
+                        ", email='" + email + '\'' +
+                        ", name=" + name +
+                        '}';
+            }
+        }
+    }
+
+

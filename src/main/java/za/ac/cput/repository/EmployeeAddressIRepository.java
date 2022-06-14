43c7d96403a1f437e4ab7695791149174de2b3d4
breySean <breytonernstzen@gmail.com>
2022-06-14 10:36:39 +0200

    Interface classes created

---
+++ src/main/java/za/ac/cput/repository/EmployeeAddressIRepository.java
@@ -0,0 +1,14 @@
+package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.entity.EmployeeAddress;
+
+/*Breyton Ernstzen (217203027)
+  ADP3 - June Assessment 2022
+  Date: 9 June 2022
+  School Management
+ */
+@Repository
+public interface EmployeeAddressIRepository extends JpaRepository<EmployeeAddress,String> {
+}

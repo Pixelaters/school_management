28368a59bbdfea776f02436faa6de1e10fab6d61
Ziyaad <Zpetersen@Bainscorp.com>
2022-06-15 11:50:42 +0200

    controller, Repository and service

--- src/main/java/za/ac/cput/entity/Country.java
+++ src/main/java/za/ac/cput/entity/Country.java
@@ -6,13 +6,14 @@ package za.ac.cput.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
 @Entity
-
+@Embeddable
 public class Country {
 
 

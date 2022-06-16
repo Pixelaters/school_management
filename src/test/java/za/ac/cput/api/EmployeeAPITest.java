1d518a08e3ce7392896e9f1f3bac005d066649d3
breySean <breytonernstzen@gmail.com>
2022-06-16 22:13:34 +0200

    changes made

--- src/test/java/za/ac/cput/api/EmployeeAPITest.java
+++ src/test/java/za/ac/cput/api/EmployeeAPITest.java
@@ -12,24 +12,21 @@ import za.ac.cput.service.EmployeeService;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class EmployeeAPITest {
+    @Autowired private EmployeeService employeeService;
 
+    @Autowired private EmployeeAPI employeeAPI;
+
+    private Employee employee = EmployeeFactory.builder("101","b@gmail.com",
+            NameFactory.buildName("Ziyaad","","Bawa"));
+    private Employee employee1 = EmployeeFactory.builder("101","b1@gmail.com",
+            NameFactory.buildName("Ziyaad","","petersen"));
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void create() {
-        @Autowired private EmployeeService employeeService;
 
-        @Autowired private EmployeeAPI employeeAPI;
-
-        private Employee employee = EmployeeFactory.builder("101","b@gmail.com",
-                NameFactory.buildName("Ziyaad","","Bawa"));
-        private Employee employee1 = EmployeeFactory.builder("101","b1@gmail.com",
-                NameFactory.buildName("Ziyaad","","petersen"));
-
-        @Test
-        void create() {
             employee = this.employeeAPI.create(this.employee);
             employee1 = this.employeeAPI.create(this.employee1);
 
@@ -39,7 +36,6 @@ class EmployeeAPITest {
 
             );
         }
-    }
 
     @Test
     void read() {
--- src/test/java/za/ac/cput/factory/EmployeeAddressFactoryTest.java
+++ src/test/java/za/ac/cput/factory/EmployeeAddressFactoryTest.java
@@ -20,17 +20,17 @@ class EmployeeAddressFactoryTest {
     //private static final Address address = new Address("");
     ArrayList<EmployeeAddress> employeeAddresses = new ArrayList<EmployeeAddress>();
     private static final EmployeeAddress ea1 = EmployeeAddressFactory.builder("101A",
-            AddressFactory.build("K18","Forest Hill","6",7100,
-                    "Searle Street", CityFactory.buildCity("333","Cape Town",
-                            CountryFactory.createCountry("South Africa","RSA-CT"))));
+            AddressFactory.build("K18","Forest Hill","6","Mexican Wave",
+                    3334, CityFactory.buildCity("333","Cape Town",
+                            CountryFactory.builder("South Africa","RSA-CT"))));
     private static final EmployeeAddress ea2 = EmployeeAddressFactory.builder("123B",
-            AddressFactory.build("E145","Bayview", "52",7306,
-                    "Bird Avenue", CityFactory.buildCity("AA1","Kimberley",
-                            CountryFactory.createCountry("South Africa","RSA-K"))));
+            AddressFactory.build("E145","Bayview", "52","Ziyaad Ave",
+                    1212, CityFactory.buildCity("AA1","Kimberley",
+                            CountryFactory.builder("South Africa","RSA-K"))));
     private static final EmployeeAddress ea3 = EmployeeAddressFactory.builder("131C",
-            AddressFactory.build("B20A","De-Zalze","12",7309,
-                    "Jan van Riebeeck Ave", CityFactory.buildCity("BF101","Bloemfontein",
-                            CountryFactory.createCountry("South Africa","RSA-B"))));
+            AddressFactory.build("B20A","De-Zalze","12","Wales",
+                    1738, CityFactory.buildCity("BF101","Bloemfontein",
+                            CountryFactory.builder("South Africa","RSA-B"))));
     private static final EmployeeAddress ea4 = ea3;
 
     @Test

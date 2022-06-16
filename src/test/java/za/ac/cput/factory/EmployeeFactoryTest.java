94cc84929b665216adb8a4d6dade912535978ff1
breySean <breytonernstzen@gmail.com>
2022-06-16 13:11:24 +0200

    additional test cases added

--- src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/factory/EmployeeFactoryTest.java
@@ -19,9 +19,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeFactoryTest {
 
     ArrayList<Employee> employee = new ArrayList<Employee>();
-    private static final Employee employee1 = EmployeeFactory.builder("101A","dix@gmail.com",null);
-    private static final Employee employee2 = EmployeeFactory.builder("123B","jinx@gmail.com",null);
-    private static final Employee employee3 = EmployeeFactory.builder("131C","217203027@mycput.ac.za",null);
+    private static final Employee employee1 = EmployeeFactory.builder("101A","dix@gmail.com",
+            NameFactory.buildName("James","John","Doe"));
+    private static final Employee employee2 = EmployeeFactory.builder("123B","jinx@gmail.com",
+            NameFactory.buildName("Dixon","","Sean"));
+    private static final Employee employee3 = EmployeeFactory.builder("131C","217203027@mycput.ac.za",
+            NameFactory.buildName("Breyton","Sean","Ernstzen"));
     private static final Employee employee4 = employee3;
 
     @Test
@@ -30,40 +33,53 @@ class EmployeeFactoryTest {
         System.out.println("Added: " + employee.get(0));
         employee.add(employee2);
         System.out.println("Added: " + employee.get(1));
-        assertNotNull(employee);
-        assertNull(employee1.getName());
-        assertNull(employee2.getName());
-        assertNull(employee3.getName());
-        assertNull(employee4.getName());
+        employee.add(employee3);
+        System.out.println("Added: " + employee.get(2));
+
+        assertAll(
+                () -> assertNotNull(employee1),
+                () -> assertNotNull(employee2),
+                () -> assertNotNull(employee3),
+                () -> assertNotNull(employee2.getName()),
+                () -> assertNotNull(employee3.getName()),
+                () -> assertNotNull(employee4.getName())
+        );
+
     }
 
     @Test
     void b_objectEquality(){
 
         //add another equality test here for three parameter inputs
-        assertNotEquals(employee1.getStaffId(),employee2.getStaffId());
-        assertNotEquals(employee1.getEmail(),employee2.getEmail());
-        assertNotEquals(employee1,employee2);
-        assertEquals(employee3,employee4);
+        assertAll(
+                () -> assertNotEquals(employee1.getStaffId(),employee2.getStaffId()),
+                () -> assertNotEquals(employee1.getEmail(),employee2.getEmail()),
+                () -> assertNotEquals(employee1,employee2),
+                () -> assertEquals(employee3,employee4),
+                () -> assertNotNull(employee1),
+                () -> assertNotNull(employee2),
+                () -> assertNotNull(employee3),
+                () -> assertNotNull(employee4)
+        );
 
-        assertNotNull(employee1);
-        assertNotNull(employee2);
-        assertNotNull(employee3);
-        assertNotNull(employee4);
         System.out.println("Equality test passed successfully");
     }
 
     @Test
     void c_objectIdentity(){
 
-        assertNotSame(employee1.getStaffId(),employee2.getStaffId());
-        assertNotSame(employee1.getEmail(),employee2.getEmail());
-        assertSame(employee3,employee4);
+        assertAll(
+                () ->  assertNotSame(employee1.getStaffId(),employee2.getStaffId()),
+                () ->  assertNotSame(employee1.getEmail(),employee2.getEmail()),
+                () ->  assertSame(employee3,employee4),
+                () ->  assertNotNull(employee1),
+                () ->  assertNotNull(employee2),
+                () ->  assertNotNull(employee3),
+                () ->  assertNotNull(employee4)
 
-        assertNotNull(employee1);
-        assertNotNull(employee2);
-        assertNotNull(employee3);
-        assertNotNull(employee4);
+        );
+
+
         System.out.println("Identity test pass successfully");
     }
 }
